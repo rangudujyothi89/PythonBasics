cc1044b1d482a012272027c26789e17af682359b
Jyothi Rangudu <ysrinivas@Srinivass-MacBook-Pro.local>
2024-07-03 00:56:41 -0400

    fix

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import math
 print("Jyothi Rangudu")
 print('*' * 10)
 #name = input('what is your name? ')
-#color = input('what is your favourite color? ') 123
+#color = input('what is your favourite color? ') 
 #print(name + ' likes ' + color)
 #Type Conversion
 int('1982')
